0afa64dce1d2cc6773fe459c4c36f3028d2d33b3
Anonymous Ikwijaya <anonymous.ikwijaya@gmail.com>
2021-09-12 01:02:38 +0700

    update

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const bodyParser = require('body-parser')
+const compression = require('compression')
 
 const { PORT } = require('./config')
 const api = require('./api/v1')
@@ -8,10 +8,11 @@ const { response } = require('./models')
 const port = PORT
 const app = express()
 
-app.use(bodyParser.json())
+app.use(compression({ filter: shouldCompress }))
+app.use(express.json())
 app.use((err, req, res, next) => {
     if (err instanceof SyntaxError) {
-        logger.error("Invalid JSON format", "body-parser", true)
+        logger.error("Invalid JSON format", "express-json", true)
         res.status(400).send(response.failed("Bad Request"))
     } else {
         next()
@@ -32,6 +33,15 @@ const authenticate = async () => {
 authenticate();
 app.use('/v1', api)
 // app.listen(port, () => { logger.info('🚀 ombudsman-api running in port ' + port, 'server', true) })  
-app.listen(process.env.PORT || 3000, function () {
+app.listen(process.env.PORT || port, function () {
     console.log("🚀 ombudsman-api running in port ' + port, 'server", this.address().port, app.settings.env);
 });
+
+// method
+function shouldCompress(req, res) {
+    if (req.headers['x-no-compression'])
+        return false
+
+    // fallback to standard filter function
+    return compression.filter(req, res)
+}
