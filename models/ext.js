bb7d09acec33fcd13a42c0b3b406b1a0f040913a
irpan.kusuma@gmail.com <irpan.kusuma@gmail.com>
2021-10-30 17:00:47 +0700

    bug fix pdf cant download/open

--- models/ext.js
+++ models/ext.js
@@ -1,8 +1,9 @@
 const supportFile = () => {
   return [
-    { type: "document", lists: ["pdf", "doc", "ppt", "xls", "docx", "pptx", "xlsx"] },
+    { type: "document", lists: ["doc", "ppt", "xls", "docx", "pptx", "xlsx"] },
     { type: "image", lists: ["jpg", "jpeg", "png"] },
-    { type: "audio", lists: ["aac", "amr", "mp3", "ogg", "opus"] }
+    { type: "audio", lists: ["aac", "amr", "mp3", "ogg", "opus"] },
+    { type: "application", lists: ["pdf"] }
   ];
 }
 
