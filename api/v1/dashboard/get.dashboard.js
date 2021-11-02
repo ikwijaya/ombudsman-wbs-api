e3d072f5c390a6443ebf2a6c072a063afcc859c1
irpan.kusuma@gmail.com <irpan.kusuma@gmail.com>
2021-11-02 20:21:28 +0700

    kosongkan object yg ga di konsumsi dan fixing object

--- api/v1/dashboard/get.dashboard.js
+++ api/v1/dashboard/get.dashboard.js
@@ -12,15 +12,15 @@ router.post('/', cache.MCache(30), async (req, res, next) => {
     let x = new WBS()
     let count_by_type = await x.getCountByType(sid).catch(e => { throw (e) })
     let count_by_status = await x.getCountByStatus(sid).catch(e => { throw (e) })
-    let to_you = []
-    let total = await x.getTotal(sid).catch(e => { throw (e) })
+    // let to_you = []
+    // let total = await x.getTotal(sid).catch(e => { throw (e) })
     // let count_by_region = await x.getCountByRegion(sid).catch(e => { throw (e) })
     // let count_by_region_name = await x.getCountByRegionName(sid).catch(e => { throw (e) })
     let complaint_by_region = await x.getComplaintByRegion(sid).catch(e => { throw (e) })
     let complaint_by_violation = await x.getComplaintByViolation(sid).catch(e => { throw (e) })
     let complaint_by_process = await x.getComplaintByProcess(sid).catch(e => { throw (e) })
     // let complaint_by_work_unit = await x.getCountByWorkUnit(sid).catch(e => { throw (e) })
-    let complaint_by_person = await x.getCountByPerson(sid).catch(e => { throw (e) })
+    // let complaint_by_person = await x.getCountByPerson(sid).catch(e => { throw (e) })
     let complaint_by_ukname1 = await x.getCountByUKName1(sid).catch(e => { throw (e) })
     let complaint_by_ukname2 = await x.getCountByUKName2(sid).catch(e => { throw (e) })
     let complaint_by_ukreg1 = await x.getCountByUKRegion1(sid).catch(e => { throw (e) })
@@ -29,17 +29,17 @@ router.post('/', cache.MCache(30), async (req, res, next) => {
     res.send({
       count_by_type: count_by_type,               // SUMBER ADUAN
       count_by_status: count_by_status,
-      to_you: to_you,
-      total: total,
+      to_you: [],
+      total: [], // total,
       count_by_region: [], //count_by_region,           // REGIONAL - PROVINSI -> REGIONAL - PERWAKILAN (Unit Kerja)
       complaint_by_region: complaint_by_region,   // Aduan Terkait Pengawasan Pelayanan Publik
       count_by_region_name: [], //count_by_region_name, // PERWAKILAN
       complaint_by_violation: complaint_by_violation.rows,  // JENIS PENGADUAN
       complaint_by_process: complaint_by_process.rows,      // PROSES KUMM
       complaint_by_work_unit: [],
-      complaint_by_person: complaint_by_person, 
+      complaint_by_person: [],  //complaint_by_person, 
       complaint_by_ukname1: complaint_by_ukname1,             // UNIT KERJA INSPEKTORAT
-      complaint_by_ukname1: complaint_by_ukname2,             // UNIT KERJA KUMM
+      complaint_by_ukname2: complaint_by_ukname2,             // UNIT KERJA KUMM
       complaint_by_ukreg1: complaint_by_ukreg1,               // INSPEKTORAT REGIONAL - UNIT KERJA (PERWAKILAN UNIT KERJA)
       complaint_by_ukreg2: complaint_by_ukreg2                // KUMM
     })
