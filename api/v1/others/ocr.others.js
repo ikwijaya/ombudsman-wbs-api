d3bbdcdc0cc0da43ebb11c9cef15d403c8def85e
irpan.kusuma@gmail.com <irpan.kusuma@gmail.com>
2021-11-04 00:46:43 +0700

    add log aneh ga baca req.body

--- api/v1/others/ocr.others.js
+++ api/v1/others/ocr.others.js
@@ -7,8 +7,10 @@ const worker = createWorker({ logger: ev => console.log('[running] ocr => ', ev)
 
 router.post('/', async (req, res, next) => {
   try {
-    let img = req.body || {}
-    if(!img.url) res.status(200).send(response.failed('Image not found!'))
+    let url = req.body.url || null
+    let filename = req.body.filename || null
+
+    if(!url) res.status(200).send(response.failed(`Image ${filename} not found!`))
     else {
       await worker.load()
       await worker.loadLanguage('eng')
@@ -16,11 +18,11 @@ router.post('/', async (req, res, next) => {
       await worker.setParameters({
         tessedit_pageseg_mode: PSM.SINGLE_BLOCK,
       })
-      let { data: { text }} = await worker.recognize(img.url)
-      img['ocr_text'] = text
-      delete img['url']
-
-      res.status(200).send(response.success('OCR generated!', img))
+      let { data: { text }} = await worker.recognize(url)
+      res.status(200).send(response.success('OCR generated!', {
+        filename: filename,
+        ocr_text: text
+      }))
     }
   } catch (err) {
     res.status(401).send(response.failed(err, []))
