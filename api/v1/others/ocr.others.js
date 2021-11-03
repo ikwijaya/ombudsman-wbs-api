bf31ae9b6ac94c58c54269428020c886fc90097e
irpan.kusuma@gmail.com <irpan.kusuma@gmail.com>
2021-11-04 00:33:10 +0700

    testing ocr from base64 image convert

--- api/v1/others/ocr.others.js
+++ api/v1/others/ocr.others.js
@@ -3,12 +3,11 @@ const router = require('express').Router()
 const { response } = require('../../../models')
 const { helper } = require('../../../helper')
 const { createWorker, PSM, OEM } = require('tesseract.js')
-const worker = createWorker({ logger: ev => console.log('ts => ', ev) })
+const worker = createWorker({ logger: ev => console.log('[running] ocr => ', ev) })
 
 router.post('/', async (req, res, next) => {
   try {
-    let img = req.body.obj || {}
-
+    let img = req.body || {}
     if(!img.url) res.status(200).send(response.failed('Image not found!'))
     else {
       await worker.load()
@@ -19,6 +18,7 @@ router.post('/', async (req, res, next) => {
       })
       let { data: { text }} = await worker.recognize(img.url)
       img['ocr_text'] = text
+      delete img['url']
 
       res.status(200).send(response.success('OCR generated!', img))
     }
