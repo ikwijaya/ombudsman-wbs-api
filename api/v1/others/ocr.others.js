201e066f12af64e89c20a5eaf295a7d00efc62a1
irpan.kusuma@gmail.com <irpan.kusuma@gmail.com>
2021-11-04 00:00:44 +0700

    add route for checking OCR images

---
+++ api/v1/others/ocr.others.js
@@ -0,0 +1,30 @@
+
+const router = require('express').Router()
+const { response } = require('../../../models')
+const { helper } = require('../../../helper')
+const { createWorker, PSM, OEM } = require('tesseract.js')
+const worker = createWorker({ logger: ev => console.log('ts => ', ev) })
+
+router.post('/', async (req, res, next) => {
+  try {
+    let img = req.body.obj || {}
+
+    if(!img.url) res.status(200).send(response.failed('Image not found!'))
+    else {
+      await worker.load()
+      await worker.loadLanguage('eng')
+      await worker.initialize('eng', OEM.LSTM_ONLY)
+      await worker.setParameters({
+        tessedit_pageseg_mode: PSM.SINGLE_BLOCK,
+      })
+      let { data: { text }} = await worker.recognize(img.url)
+      img['ocr_text'] = text
+
+      res.status(200).send(response.success('OCR generated!', img))
+    }
+  } catch (err) {
+    res.status(401).send(response.failed(err, []))
+  }
+});
+
+module.exports = router
