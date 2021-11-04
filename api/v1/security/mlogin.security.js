4d12bb92ccb53202bb2e6d42c867e3c643e3587a
irpan.kusuma@gmail.com <irpan.kusuma@gmail.com>
2021-11-04 21:28:29 +0700

    add route login for mobile apps

---
+++ api/v1/security/mlogin.security.js
@@ -0,0 +1,34 @@
+
+const router = require('express').Router()
+const { response } = require('../../../models')
+const { security } = require('../../../sequelize/controllers')
+const { hmac } = require('../../../helper')
+
+/**
+ * Route
+ * Transaction Search
+ */
+router.post('/', async (req, res, next) => {
+  let email = req.body.email || null;
+  let password = req.body.password || null;
+  let ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress 
+  let ua = req.headers['user-agent'] || null
+  let host = req.headers.host || null
+
+  try {
+    if (!email || password) {
+      res.status(200).send(response.failed('Kolom Email dan Password TIDAK boleh kosong.'))
+    } else {
+      let o = await security.login(email, password, {
+        ua: ua,
+        ip: ip,
+        host: host
+      }).catch(e => { throw (e) })
+      res.send(o).status(200);
+    }
+  } catch (err) {
+    res.status(401).send(response.failed(err, []))
+  }
+});
+
+module.exports = router
