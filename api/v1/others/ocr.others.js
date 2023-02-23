91f58831ac37d0399be5e110efb00d9917af9198
irpan.kusuma@gmail.com <irpan.kusuma@gmail.com>
2023-02-23 21:09:52 +0700

    fixing error

--- api/v1/others/ocr.others.js
+++ api/v1/others/ocr.others.js
@@ -5,7 +5,7 @@ const { helper } = require('../../../helper')
 const CPM = require('connect-multiparty')
 const multiparty = CPM();
 const { createWorker, PSM, OEM } = require('tesseract.js')
-const worker = createWorker({ 
+const worker = createWorker({
   // logger: ev => console.log('[running] ocr => ', ev),
   errorHandler: (err) => { return err }
 })
@@ -16,28 +16,33 @@ router.post('/', multiparty, async (req, res, next) => {
     let url = upload.path;
     let filename = upload.originalFilename
 
-    if(!url) res.status(200).send(response.failed(`Image ${filename} not found!`))
-    else if(upload.size/1000 > 2000) res.status(200).send(response.failed(`Gambar Tidak lebih dari 1MB`))
+    // console.log(` ${upload.size / 1000}`)
+
+    if (!url) res.status(200).send(response.failed(`Image ${filename} not found!`))
+    else if (upload.size / 1000 > 5000) res.status(200).send(response.failed(`Gambar Tidak lebih dari 1MB`))
     else {
-      await worker.load().catch(e => {throw (e) })
-      await worker.loadLanguage('ind').catch(e => {throw (e) })
-      await worker.initialize('ind', OEM.LSTM_ONLY).catch(e => {throw (e) })
+      await worker.load().catch(e => { throw (e) })
+      await worker.loadLanguage('ind').catch(e => { throw (e) })
+      await worker.initialize('ind', OEM.LSTM_ONLY).catch(e => { throw (e) })
       await worker.setParameters({
         tessedit_pageseg_mode: PSM.SINGLE_BLOCK,
-      }).catch(e => {throw (e) })
-      let { data: { text }} = await worker.recognize(url).catch(e => {throw (e) })
+      }).catch(e => { throw (e) })
+      let { data: { text } } = await worker.recognize(url).catch(e => { throw (e) })
 
       /**
        * final
        */
-      if(!text.match(/NIK/g) && !text.match(/warganegara/g)) res.status(200).send(response.failed(`Mohon meng-upload hanya KTP.`))
+      const arr = !text ? null : text.split("\n")
+      const c = await extract(arr);
+      if (!c) res.status(200).send(response.failed(`Kami TIDAK dapat memindai gambar dengan baik, karena pencahayaan pada gambar kurang.`))
+      else if (!c.nik) res.status(200).send(response.failed(`Kami TIDAK dapat memindai gambar dengan baik, karena pencahayaan pada gambar kurang.`))
       else {
-        const arr = !text ? null : text.split("\n") 
         res.status(200).send(response.success('OCR generated!', {
           filename: filename,
           ocr: {
             // text: text,
-            data: await extract(arr), 
+            arr:  process.env.NODE_ENV.match(/dev/g) ? arr : [],
+            data: await extract(arr),
           }
         }))
       }
@@ -54,16 +59,20 @@ module.exports = router
  * @param {*} arr 
  * @returns 
  */
-const extract = async (arr=[]) => {
+const extract = async (arr = []) => {
   return new Promise((ok) => {
-    if(arr.length == 0) return ok(null)
+    if (arr.length == 0) return ok(null)
 
     let o = {}
     arr.forEach(e => {
-      if(e.includes('NIK')) o['identityType'] = 'KTP'
-      else if(e.includes('DRIVING LICENSE')) o['identityType'] = 'SIM'
+      if (e.includes('NIK')) o['identityType'] = 'KTP'
+      else if (e.includes('DRIVING LICENSE')) o['identityType'] = 'SIM'
 
-      if(e.includes('NIK')) o['nik'] = e.replace(/([A-Za-z])|(\s+)|([:?\-:&*%^()$#@!_+=~`])/g,'');
+      if (e.match(/(NIK|nik)/g)) {
+        const nik = e.replace(/([A-Za-z])|(\s+)|([:?\-:&*%^()$#@!_+=~`])/g, '');
+        o['nik'] = nik
+        console.log(`nik`, nik) 
+      }
     });
 
     return ok(o)
